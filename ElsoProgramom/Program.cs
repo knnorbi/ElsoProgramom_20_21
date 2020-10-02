c518874cac84fd9649bf6d8113072cc538edcec6
knnorbi <kluka.norbert@gmail.com>
2020-10-02 14:21:58 +0200

    Initial commit

---
+++ ElsoProgramom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElsoProgramom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Deklaráció
+            int a;
+            a = 5;
+            Console.WriteLine(a);
+
+            int b = 7;
+            Console.WriteLine(b);
+
+            int c = 3 * a + (a * b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("Mikor születtél?");
+            string beolvasott = Console.ReadLine();
+            Console.WriteLine("Ezt írtad be: " + beolvasott);
+            int beolvasottSzamkent = int.Parse(beolvasott);
+            int eves = 2020 - beolvasottSzamkent;
+            Console.WriteLine(eves + " éves vagy.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
